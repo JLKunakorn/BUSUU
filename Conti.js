f431499717d3097b4886d41b3297c1bedb2bbdc0
JLKunakorn <157047365+JLKunakorn@users.noreply.github.com>
2025-05-01 17:06:26 +0700

    Delete Conti.js

--- Conti.js
+++
@@ -1,8 +0,0 @@
-const continueBtn = document.querySelector('button.ex-feedback-bar__button');
-
-if (continueBtn) {
-  continueBtn.click();
-  console.log('✅ Clicked Continue button.');
-} else {
-  console.warn('⚠️ Continue button not found.');
-}
